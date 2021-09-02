500b604786272a84e0ebc7a4995688c7b7155df6
Martin Nguyen <martin@maddin.io>
2021-09-02 16:32:40 +0900

    Added Swift Collections as a dependency.

--- Package.swift
+++ Package.swift
@@ -22,14 +22,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        //.package(url: "https://github.com/evgenyneu/keychain-swift", from: "19.0.0"),
+        .package(url: "https://github.com/apple/swift-collections", from: "0.0.7"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WireKit",
-            dependencies: []),
+            dependencies: [.product(name: "OrderedCollections", package: "swift-collections")]),
         .testTarget(
             name: "WireKitTests",
             dependencies: ["WireKit"],
